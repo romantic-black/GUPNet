a5f7e7c743e6be81c38936a79bab499b768692b8
root <1459282203@qq.com>
2024-01-16 13:48:30 +0800

    aug init

--- code/tools/train_val.py
+++ code/tools/train_val.py
@@ -3,6 +3,8 @@ import sys
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(BASE_DIR)
 sys.path.append(ROOT_DIR)
+sys.path = sys.path[:-1]
+print(sys.path)
 
 import yaml
 import logging
